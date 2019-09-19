b31cb4a5338a5da23cdf767c30e7d9d26f947013
CN105 <cn105@CN0105.local>
2019-09-19 19:02:32 +0100

    added fontawsome css

---
+++ public/README.txt
@@ -0,0 +1,12 @@
+
+https://darksky.net/dev/docs/faq
+
+"Where do I find the icons?
+The API itself doesn't come with any icons: instead, 
+we provide a generic icon property so you can select whatever icons 
+you like! If you want to use the same icon set we use on darksky.net, 
+ut our Skycons library. We also really like Adam Whitcroft's Climacons."
+
+http://adamwhitcroft.com/climacons/
+
+http://darkskyapp.github.io/skycons/
--- public/index.js
+++ public/index.js
@@ -2,21 +2,29 @@
 console.log('weather app working')
 
 const title = document.getElementById('title')
-const getLocation = document.getElementById('location')
+const getLocation = document.getElementById('getLocation')
 const fetchWeather = document.getElementById('fetchWeather')
 const summary = document.getElementById('summary')
 const temp = document.getElementById('temp')
-
+const placeFound = document.getElementById('placeFound')
+const loading = document.getElementById('loading')
 
 
 fetchWeather.addEventListener('click', async ()=>{
 
-    summary.textContent = "Loading weather ..."
+    
+    loading.innerHTML = '<i class="fas fa-spinner"></i>'
     temp.textContent = ""
 
     let responce = await fetch(`http://localhost:3001/weather?location=${getLocation.value}`)
     let data = await responce.json()
-    summary.textContent = data.summary
-    temp.textContent = `${Math.floor(((data.temperature) -32) * (5/9))} C`
+
+    loading.innerHTML = ""
+    iconDisplay.innerHTML = '<i class="far fa-sun"></i>'
+    placeFound.textContent = data.placeInfo
+    summary.textContent = data.weatherInfo.summary
+
+    temp.innerHTML = `${Math.floor(((data.weatherInfo.temperature) -32) * (5/9))}`+ '<i class="fas fa-temperature-high"></i>'
+
 })
 
