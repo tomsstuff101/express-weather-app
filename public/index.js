499724ffbea649125b6135e5be1a95fd6dab75dd
CN105 <cn105@CN0105.local>
2019-10-14 09:01:27 +0100

    adapting for templating

--- public/index.js
+++ public/index.js
@@ -1,31 +1,31 @@
 // Client side JavaScript here
-console.log('weather app working')
+// console.log('weather app working')
 
-const title = document.getElementById('title')
-const getLocation = document.getElementById('getLocation')
-const fetchWeather = document.getElementById('fetchWeather')
-const summary = document.getElementById('summary')
-const temp = document.getElementById('temp')
-const placeFound = document.getElementById('placeFound')
-const loading = document.getElementById('loading')
+// const title = document.getElementById('title')
+// const getLocation = document.getElementById('getLocation')
+// const fetchWeather = document.getElementById('fetchWeather')
+// const summary = document.getElementById('summary')
+// const temp = document.getElementById('temp')
+// const placeFound = document.getElementById('placeFound')
+// const loading = document.getElementById('loading')
 
 
-fetchWeather.addEventListener('click', async ()=>{
+// fetchWeather.addEventListener('click', async ()=>{
 
     
-    loading.innerHTML = '<i class="fas fa-spinner"></i>'
-    temp.textContent = ""
+//     loading.innerHTML = '<i class="fas fa-spinner"></i>'
+//     temp.textContent = ""
 
-    // let responce = await fetch(`http://localhost:3001/weather?location=${getLocation.value}`)
-    let responce = await fetch(`/weather?location=${getLocation.value}`)
-    let data = await responce.json()
+//     // let responce = await fetch(`http://localhost:3001/weather?location=${getLocation.value}`)
+//     let responce = await fetch(`/weather?location=${getLocation.value}`)
+//     let data = await responce.json()
 
-    loading.innerHTML = ""
-    iconDisplay.innerHTML = '<i class="far fa-sun"></i>'
-    placeFound.textContent = data.placeInfo
-    summary.textContent = data.weatherInfo.summary
+//     loading.innerHTML = ""
+//     iconDisplay.innerHTML = '<i class="far fa-sun"></i>'
+//     placeFound.textContent = data.placeInfo
+//     summary.textContent = data.weatherInfo.summary
 
-    temp.innerHTML = `${Math.floor(((data.weatherInfo.temperature) -32) * (5/9))}`+ '<i class="fas fa-temperature-high"></i>'
+//     temp.innerHTML = `${Math.floor(((data.weatherInfo.temperature) -32) * (5/9))}`+ '<i class="fas fa-temperature-high"></i>'
 
-})
+// })
 
--- server.js
+++ server.js
@@ -1,24 +1,42 @@
 // setup
 const express = require('express')
 const path = require('path')
-const {main} = require('./lib/app')
+// const {main} = require('./lib/app')
+const hbs = require("express-handlebars")
+const bodyParse = require("body-parser")
+
 const port = process.env.PORT || 3001
 
+const routes = require("./routes/index")
 
 const app = express()
 
 // public static directory
 // serve via 'use' a static 
 app.use(express.static(path.join(__dirname, "public")))
+app.use(bodyParser.urlencoded({ encoded: false }))
+app.use(bodyParser.json())
 
 
-// make sure you await for the info from the api's 
-// before sending the data to the client
-app.get ('/weather' , async (req,res) =>{
+app.engine(
+    ".hbs",
+    hbs({
+        defaultLayout: "layout",
+        extname: ".hbs"
+    })
+)
 
-    let data = await main(req.query.location)
-    res.send(data)
-})
+app.set("view engine", ".hbs")
+
+app.use("/", routes)
+
+// // make sure you await for the info from the api's 
+// // before sending the data to the client
+// app.get ('/weather' , async (req,res) =>{
+
+//     let data = await main(req.query.location)
+//     res.send(data)
+// })
 
 
 app.listen(port, ()=>{
